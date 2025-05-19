b5257df49c046a654f6e961b3da768a9b7b72faa
Ian MacDonald Spence <50440569+IanMacSpence@users.noreply.github.com>
2025-05-19 12:11:45 +0200

    Fix score ID errors

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-//write function that adds to the score of the team based on the button clicked and arguments passed
-function addPoints(team, points){
-    const scoreEl = document.getElementById(`${team}-score`);
-    let currentScore = parseInt(scoreEl.textContent, 10);
-    scoreEl.textContent = currentScore + points;
+// write function that adds to the score of the team based on the button clicked and arguments passed
+function addPoints(team, points) {
+  const scoreEl = document.getElementById(`${team}-score`);
+  let currentScore = parseInt(scoreEl.textContent, 10);
+  scoreEl.textContent = currentScore + points;
 }
 
-//function that creates a new game by resetting the scores
-function newGame(){
-    document.getElementById("home-score").textContent = 0;
-    document.getElementById("guest-score").textContent = 0;
+// function that creates a new game by resetting the scores
+function newGame() {
+  document.getElementById("home-score").textContent = 0;
+  document.getElementById("guest-score").textContent = 0;
 }
-
